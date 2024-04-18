8e56e70562da790e6259f1268b5089fb1ec253a2
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2024-04-19 05:56:20 +0700

    feat: add stop triggering if some action completed ( dynamic route soon )

--- app.ml
+++ app.ml
@@ -1,13 +1,24 @@
-let run routes =
-  Dream.run ~interface:"0.0.0.0" @@ Dream.logger @@ Dream.router
-    (Seele.process_route routes)
-
-let routes = [ ("/", Home.render); ("static", Dream_html.HTML.null []) ]
-
-let main () =
+let run routes ?stop_condition () =
+  match stop_condition with
+  | None ->
+     Dream.run ~interface:"0.0.0.0"
+     @@ Dream.logger
+     @@ Dream.router (Seele.process_route routes)
+  | Some condition ->
+     Dream.run ~interface:"0.0.0.0" ~stop:condition
+     @@ Dream.logger
+     @@ Dream.router (Seele.process_route routes)
+ 
+ let routes =
+  [
+     ("/", Home.render);
+     ("blog/something", Home.render);
+     ("blog/reborn", Home.render);
+     ("static", Dream_html.HTML.null []);
+  ]
+ 
+ let () =
   if Seele.is_env_dev then
-    run routes
+     run routes ()
   else
-    Lwt_main.run (Seele.export_to_html routes)
-
-let () = main ()
+     run routes ~stop_condition:(Seele.export_to_html routes) ()
