50fe97186bccac666dfa82762d7f80c2637906ba
Fahmi Irsyad Khairi <fahmiirsyad10@gmail.com>
2024-04-25 14:40:53 +0700

    feat: update url

--- pages/home.ml
+++ pages/home.ml
@@ -8,18 +8,21 @@ let page =
         [ txt "Hello, World! %s" Sys.ocaml_version ];
     ]
 
-let blog list_blog =
+let rec elements =
   let open Dream_html in
   let open HTML in
-  let list_blog = List.map (fun x ->
-    li [ class_ "text-2xl font-bold text-red-400 text-center" ] [ txt x ]
-  ) list_blog in
-  null
+  function
+  | [] -> []
+  | x :: xs -> li [] [ a [ href {|/blog/%s|} x ] [ txt "%s" x ] ] :: elements xs
+
+let blog (list_blog: string list) =
+  let open Dream_html in
+  let open HTML in
+  div
+    [ class_ "container mx-auto" ]
     [
-      h1
-        [ class_ "text-4xl font-bold text-red-400 text-center" ]
-        [ txt "Hello, World! %s" Sys.ocaml_version ];
-      ul [] list_blog;
+      h1 [ class_ "text-4xl font-bold text-red-400 text-center" ] [ txt "Blog" ];
+      ul [] (elements list_blog);
     ]
 
 let render = Default.base ~titles:"fahmiirsyadk" page
